8d421eb2d7e4e27b9db8f1d5f8413428d730d021
rpolicastro <rpolicas@indiana.edu>
2019-07-14 10:38:11 -0400

    adding small script to grab singularity container

---
+++ singularity/download_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONTAINER="singularity pull library://rpolicastro/default/stripe_seq:1.0.0"
+
+#########################################
+## Download Container From Syslabs Cloud
+#########################################
+
+singularity pull library://rpolicastro/default/stripe_seq:1.0.0
