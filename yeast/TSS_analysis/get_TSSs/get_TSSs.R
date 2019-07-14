3168957f5611ba7110541a45e811a9f400d359d6
rpolicastro <rpolicas@indiana.edu>
2019-07-13 20:16:59 -0400

    started writing script to extract TSSs

---
+++ yeast/TSS_analysis/get_TSSs/get_TSSs.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+
+############
+## Get TSSs
+############
+
+## TSRchitect to Extract TSSs
+## ----------
+
+## Load TSS object.
+
+tss.obj <- loadTSSobj(
+	experimentTitle="Yeast STRIPE-seq",
+	inputDir="../../aligned_reads/",
+	n.cores <- 4,
+	isPairedBAM=TRUE,
+	isPairedBED=FALSE,
+	sampleSheet="sample_sheet.tsv",
+	sampleNames="",
+	replicateIDs=""
+)
+
+## Get fragment 5` ends.
+
+tss.obj <- inputToTSS(tss.obj)
+
+## Find TSSs.
+
+tss.obj <- processTSS(
+	tss.obj,
+	n.cores=4,
+	tssSet="all",
+	writeTable=FALSE
+)
+
+## Pull TSSs from TSRchitect Object and Export
+## ----------
