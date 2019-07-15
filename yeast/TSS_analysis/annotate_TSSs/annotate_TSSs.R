bcb0f1326a8ca9eadaa9c8043b4f8e25dc4bdfee
rpolicastro <rpolicas@indiana.edu>
2019-07-14 20:50:27 -0400

    making explicit analysis of annotating promoters

---
+++ yeast/TSS_analysis/annotate_TSSs/annotate_TSSs.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+
+library("GenomicRanges")
+library("GenomicFeatures")
+library("ChIPseeker")
+library("tidyverse")
+
+####################################
+## Get Promoter Proximal Percentage
+####################################
+
+## Annotate TSSs
+## ----------
+
+## Load TSS data.
+
+TSSs <- readRDS("../get_TSSs/Yeast_TSSs.RDS")
+
+## Load genomic annotation.
+
+annotation <- makeTxDbFromGFF(
+	"../../genome/Saccharomyces_cerevisiae.R64-1-1.97.gtf",
+	format="gtf"
+)
+
+## Annotate TSSs.
+
+TSSs.annotated <- map(
+	TSSs,
+	~ annotatePeak(.,
+		tssRegion=c(-1000,100),
+		TxDb=annotation,
+		sameStrand=TRUE
+	) %>%
+	as_tibble
+)
+
+## Export Annotated TSSs
+## ----------
+
+##  Export to RDS.
+
+saveRDS(TSSs.annotated, "TSSs_annotated.RDS")
+
+##  Save to file.
+
+dir.create("annotated_TSSs")
+
+map(
+	names(TSSs.annotated),
+	~ write.table(
+		TSSs.annotated[[.]], file.path("annotated_TSSs", paste0("Annotated-TSSs_", ., ".tsv")),
+		sep="\t", col.names=T, row.names=F, quote=F, na=""
+	)
+)
