a9bd6207c6937f0ff1691027174d53f6f78b3863
rpolicastro <rpolicas@indiana.edu>
2019-07-14 21:27:04 -0400

    adding promoter plots

--- yeast/TSS_analysis/promoter_proximal/promoter_proximal.R
+++ yeast/TSS_analysis/promoter_proximal/promoter_proximal.R
@@ -67,7 +67,16 @@ promoter.data <- promoter.data %>% dplyr::select(
 	perc.unique, n.total, perc.total, n.genes
 )
 
-## Export results.
+## Export Results
+## ----------
+
+## Export to RDS.
+
+promoter.data %>%
+	group_split(sample) %>%
+	saveRDS(., "Promoter_Data.RDS")
+
+## Export to files.
 
 dir.create("promoter_data")
 
@@ -79,3 +88,31 @@ promoter.data %>%
 			sep="\t", col.names=T, row.names=F, quote=F
 		)
 	)
+
+## Plot Promoter Proximal
+## ----------
+
+## Split data.
+
+promoter.data.split <- group_split(promoter.data, sample)
+
+## Plot data.
+
+dir.create("promoter_plots")
+
+plot.promoters <- function(x) {
+	p <- ggplot(x, aes(x=threshold, y=perc.unique)) +
+		geom_line() +
+		geom_point(aes(size=n.genes, color=n.genes)) +
+		scale_color_viridis_c() +
+		ggtitle(x %>% pull(sample) %>% unique) +
+		theme(text=element_text(size=24))
+
+	file.name <- paste0("Promoter-Plot_", (x %>% pull(sample) %>% unique), ".png")
+	
+	png(file.path("promoter_plots", file.name), width=850, height=480)
+	print(p)
+	dev.off()
+}
+
+map(promoter.data.split, ~plot.promoters(.))
