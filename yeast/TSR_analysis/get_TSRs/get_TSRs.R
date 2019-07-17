b9a6519556aca205f2b130a2636ad2debb8e81b7
rpolicastro <rpolicas@indiana.edu>
2019-07-17 09:52:39 -0400

    finished retrieval of TSRs

--- yeast/TSR_analysis/get_TSRs/get_TSRs.R
+++ yeast/TSR_analysis/get_TSRs/get_TSRs.R
@@ -8,11 +8,58 @@ library("GenomicRanges")
 ## Find TSRs
 #############
 
-## Prepare Files for Analysis
+## Prepare Data
 ## ----------
 
-## Get files.
+## Load TSS GRanges.
 
-bam.files <- list.files("../../aligned_reads", pattern=".*\\.bam$")
+TSSs <- readRDS("../../TSS_analysis/get_TSSs/Yeast_TSSs.RDS")
 
-sample.names <- str_replace(bam.files, "_Aligned.out_cleaned.bam", "")
+## Load determined thresholds.
+
+source("threshold_values.conf")
+
+## Filter TSSs by Threshold
+## ----------
+
+## Filter TSSs.
+
+TSSs.filtered <- map(
+	names(TSSs),
+	~TSSs[[.]][score(TSSs[[.]]) >= threshold.values[[.]]]
+) %>% setNames(names(TSSs))
+
+## Export filtered TSSs.
+
+saveRDS(TSSs.filtered, "Filtered_TSSs.RDS")
+
+## Merge TSSs
+## ----------
+
+## Function to merge TSSs and get stats.
+
+merge.TSSs <- function(x) {
+	merged.TSSs <- reduce(
+		x,
+		ignore.strand=FALSE,
+		min.gapwidth=26L,
+		with.revmap=TRUE
+	)
+	mcols(merged.TSSs) <- aggregate(
+		x,
+		merged.TSSs$revmap,
+		score=sum(score),
+		nTSSs=lengths(seqnames)
+	)
+	merged.TSSs$grouping <- NULL
+	return(merged.TSSs)
+}
+
+## Merge TSSs within 25 bases.
+
+TSRs <- map(TSSs.filtered, ~merge.TSSs(.))
+
+## Export TSRs
+## ----------
+
+saveRDS(TSRs, "TSRs.RDS")
