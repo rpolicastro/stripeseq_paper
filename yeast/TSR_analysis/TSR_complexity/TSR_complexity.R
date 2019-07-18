15c8923e883b329baacde52dce332bc458c7400e
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-18 09:09:02 -0400

    TSR complexity analysis (unique TSSs per TSR)

---
+++ yeast/TSR_analysis/TSR_complexity/TSR_complexity.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+library("tidyverse")
+
+####################################################
+## Measures of TSR Complexity (Unique TSSs per TSR)
+####################################################
+
+## Load and Prepare Data
+## ----------
+
+## Load TSR data.
+
+TSRs <- readRDS("../TSR_distribution/TSR_data.RDS")
+
+## Fix factor levels on data.
+
+TSRs <- map(
+	TSRs,
+	~mutate(., cleaned.annotations=factor(
+		cleaned.annotations,
+		levels=c("promoter", "exon", "intron", "downstream", "intergenic")
+	))
+)
+
+## Plot TSR Complexity
+## ----------
+
+## Make outpit directory.
+
+dir.create("complexity_plots")
+
+## Function to make TSR complexity histogram.
+
+plot.complexity <- function(x) {
+	p <- ggplot(TSRs[[x]], aes(x=cleaned.annotations, y=log2(nTSSs+1))) +
+		geom_jitter(color="grey", size=0.25, width=0.25) +
+		geom_boxplot(fill=NA, aes(color=cleaned.annotations), outlier.shape=NA, lwd=1, width=0.25) +
+		scale_color_viridis_d() +
+		theme_bw() +
+		ylim(0,NA) +
+		theme(text=element_text(size=18))
+
+	png(
+		file.path("complexity_plots", paste0("TSR-Complexity-Plot_", x, ".png")),
+		width=850, height=600
+	)
+	print(p)
+	dev.off()
+}
+
+## Plot TSR complexity.
+
+map(names(TSRs), ~plot.complexity(.))
