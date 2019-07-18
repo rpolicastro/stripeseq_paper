f6ad36af8ef6c3be5755bd7bd9d17c0cfabb36d2
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-18 12:08:53 -0400

    finishing TSR shape and intensity plots

--- yeast/TSR_analysis/TSR_stats/TSR_stats.R
+++ yeast/TSR_analysis/TSR_stats/TSR_stats.R
@@ -20,13 +20,45 @@ TSRs.cleaned <- TSRs %>%
 		~dplyr::select(., -geneId, -transcriptId, -annotation, -cleaned.annotations) %>%
 		mutate(
 			log2_nTAGs = log2(nTAGs),
-			log2_nTSSs = log2(nTSSs),
-			log2_tsrWdth = log2(tsrWdth)
+			log2_nTSSs = log2(nTSSs+1),
+			log2_tsrWdth = log2(tsrWdth+1)
 		) %>%
-		gather(key="stat", value="stat_value")
+		dplyr::select(-nTAGs, -nTSSs, -tsrWdth) %>%
+		gather(key="stat", value="stat_value") %>%
+		mutate("sample"="samp")
 	)
 
 ## Plotting Data
 ## ----------
 
+## Create output directory.
 
+dir.create("TSR_stat_plots")
+
+## Function to plots TSR stats.
+
+plot.tsr.stats <- function(x) {
+	p <- ggplot(TSRs.cleaned[[x]], aes(x=sample, y=stat_value, color=stat)) +
+		geom_jitter(size=0.1) +
+		facet_wrap(~stat, ncol=3, scales="free") +
+		scale_color_viridis_d() +
+		geom_boxplot(color="#1c1c1c", width=0.25, fill=NA, outlier.shape=NA) +
+		theme(
+			axis.text.x=element_blank(),
+			legend.position="none",
+			axis.title.x=element_blank(),
+			axis.title.y=element_blank(),
+			text=element_text(size=18)
+		)
+
+	png(
+		file.path("TSR_stat_plots", paste0("TSR-Stat-Plot_", x, ".png")),
+		height=600, width=450
+	)
+	print(p)
+	dev.off()
+}
+
+## Plot TSR stats.
+
+map(names(TSRs.cleaned), ~plot.tsr.stats(.))
