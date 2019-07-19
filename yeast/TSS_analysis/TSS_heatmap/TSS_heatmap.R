92f61372a75fe9b53b6dd1dec338f19b47d598a8
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-19 14:34:48 -0400

    finishing TSS heatmaps

--- yeast/TSS_analysis/TSS_heatmap/TSS_heatmap.R
+++ yeast/TSS_analysis/TSS_heatmap/TSS_heatmap.R
@@ -18,7 +18,10 @@ TSSs <- readRDS("../annotate_TSSs/TSSs_annotated.RDS")
 TSSs <- map(
 	TSSs, 
 	~dplyr::select(., geneId, distanceToTSS, score) %>%
-		filter(distanceToTSS >= -1000 & distanceToTSS <= 1000) %>%
+		filter(
+			(distanceToTSS >= -1000 & distanceToTSS <= 1000),
+			score >= 3
+		) %>%
 		group_by(geneId, distanceToTSS) %>%
 		summarize(score=sum(score)) %>%
 		ungroup %>%
@@ -32,7 +35,7 @@ TSSs <- map(
 
 count.matrix <- TSSs %>%
 	map(., 
-		~spread(., key=distanceToTSS, value=score) %>%
+		~spread(., key="distanceToTSS", value="score") %>%
 		bind_rows %>%
 		replace(., is.na(.), 0)
 	)
@@ -48,7 +51,10 @@ row.order <- count.matrix %>%
 		pull(geneId)
 	)
 
-count.matrix <- map2(count.matrix, row.order, ~.x[match(.y, .x$geneId),])
+count.matrix <- map2(
+	count.matrix, row.order,
+	~.x[match(.y, .x$geneId),]
+)
 
 ## Export Count Matrix
 ## ----------
@@ -72,3 +78,46 @@ map(
 		sep="\t", col.names=T, row.names=F, quote=F
 	)
 )
+
+## Plotting Data
+## -----------
+
+## Formatting data for plotting.
+
+to.plot <- map(
+	count.matrix,
+	~mutate_if(., is.numeric, ~log2(.+1)) %>%
+		gather(key="Position", value="log2_score", -geneId) %>%
+		mutate(Position=as.integer(Position)) %>%
+		dplyr::rename("Gene"=geneId)
+)
+
+## Create output directory.
+
+dir.create("TSS_heatmaps")
+
+## Function for plotting data.
+
+plot.TSS.heatmap <- function(x) {
+	p <- to.plot[[x]] %>%
+		filter(Position >= -250 & Position <= 250) %>%
+		ggplot(aes(x=Position, y=Gene, fill=log2_score)) +
+			geom_tile() +
+			theme_minimal() +
+			theme(
+				axis.text.y=element_blank(),
+				panel.grid=element_blank()
+			) +
+			scale_fill_viridis_c(limits=c(0,4)) +
+			geom_vline(xintercept=0, lty=2, color="white") +
+			labs(fill="log2(Score + 1)")
+
+	ggsave(
+		file.path("TSS_heatmaps", paste0("TSS-heatmap_", x, ".png")),
+		plot=p, device="png", height=4, width=4
+	)
+}
+
+## Plot heatmaps.
+
+map(names(to.plot), ~plot.TSS.heatmap(.))
