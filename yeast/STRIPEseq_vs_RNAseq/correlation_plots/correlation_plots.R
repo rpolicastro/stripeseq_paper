f3d90baa8eb032fdde1b7ebaabab59ac9b992a29
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-23 13:41:01 -0400

    adding correlation matrix plot for RNA-seq versus STRIPE-seq

--- yeast/STRIPEseq_vs_RNAseq/correlation_plots/correlation_plots.R
+++ yeast/STRIPEseq_vs_RNAseq/correlation_plots/correlation_plots.R
@@ -62,10 +62,33 @@ dir.create("spearmans_corr")
 ## Calculate Spearman's rho.
 
 correlation <- TMM %>%
+	as.matrix %>%
 	cor(method="spearman") %>%
-	as_tibble(rownames="comparisons")
+	as_tibble(rownames="sample_1") %>%
+	gather(key="sample_2", value="spearman", -sample_1) %>%
+	mutate(spearman=round(spearman, 3))
 
-## Export results.
+## Plot correlation matrix.
+
+p <- ggplot(correlation, aes(x=sample_1, y=sample_2, fill=spearman, label=spearman)) +
+	geom_tile(color="white", lwd=0.5) +
+	geom_label(color="white", label.size=NA, fill=NA) +
+	scale_fill_viridis_c(limits=c(0.7,1), name="Spearman") +
+	theme_minimal() +
+	theme(
+		axis.text.x=element_text(angle=45, hjust=1),
+		panel.grid=element_blank(),
+		axis.title=element_blank()
+	)
+
+dir.create("corr_matrix_plot")
+
+ggsave(
+	file.path("corr_matrix_plot", "corr_matrix_plot.pdf"),
+	plot=p, device="pdf", height=5.5, width=7
+)
+
+## Export text results.
 
 write.table(
 	correlation, file.path("spearmans_corr", "spearmans_correlation_matrix.tsv"),
