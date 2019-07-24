60fa2ba0a36279d31a5cb5710fca3b6ee2b81a76
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-24 15:35:25 -0400

    TSR correlation

--- yeast/TSR_analysis/TSR_correlation/TSR_correlation.R
+++ yeast/TSR_analysis/TSR_correlation/TSR_correlation.R
@@ -3,6 +3,7 @@
 library("GenomicRanges")
 library("tidyverse")
 library("gtools")
+library("edgeR")
 
 ######################################
 ## TSR Correlation Between Replicates
@@ -16,49 +17,91 @@ TSRs <- readRDS("../get_TSRs/TSRs.RDS")
 ## Merge TSRs
 ## ----------
 
+merged.ranges <- TSRs %>%
+	as("GRangesList") %>%
+	unlist %>%
+	GenomicRanges::reduce(ignore.strand=FALSE)
 
+names(merged.ranges) <- sprintf("TSR_%s", 1:length(merged.ranges))
 
 ## Get Overlapping Regions
 ## ----------
 
+return.overlaps <- function(x) {
+	overlaps <- findOverlaps(query=merged.ranges, subject=TSRs[[x]]) %>%
+                as.tibble %>%
+                mutate(
+                        nTAGs=TSRs[[x]][subjectHits]$nTAGs,
+                        TSR_name=names(merged.ranges[queryHits])
+                ) %>%
+                dplyr::select(-queryHits, -subjectHits) %>%
+                group_by(TSR_name) %>%
+                summarize(nTAGs=sum(nTAGs))
+
+	return(overlaps)
+}
+
+overlapping <- map(names(TSRs), ~return.overlaps(.))
+names(overlapping) <- names(TSRs)
+
+## TMM Normalize Reads
+## ----------
+
+## Make count matrix.
+
+count.matrix <- overlapping %>%
+	bind_rows(.id="sample") %>%
+	spread(key=sample, value=nTAGs, fill=0) %>%
+	column_to_rownames("TSR_name") %>%
+	as.matrix
+
+## Get TMM normalized reads.
+
+log2.TMM <- count.matrix %>%
+	DGEList %>%
+	calcNormFactors %>%
+	cpm %>%
+	as_tibble(rownames="TSR_name") %>%
+	mutate_if(is.numeric, ~log2(.+1))
+
+## Correlation Plot
+## ----------
+
 ## Comparison combinations.
 
-sample.combinations <- TSRs %>%
+# Clean up sample names.
+names(log2.TMM) <- log2.TMM %>%
+	names %>%
+	str_replace_all("-", "_")
+
+# Get combinations.
+sample.combinations <- log2.TMM %>%
 	names %>%
 	combinations(6, 2, ., repeats.allowed=FALSE) %>%
 	as_tibble %>%
 	dplyr::rename("sample_1"=1, "sample_2"=2)
 
-## Jaccard Index
-## ----------
+## Create output directory.
 
-## Bin TSRs into deciles.
+dir.create("TSR_correlation")
 
-TSRs.decile <- map(
-	TSRs,
-	~as_tibble(.) %>%
-		mutate(decile=ntile(nTAGs,10)) %>%
-		makeGRangesFromDataFrame(keep.extra.columns=TRUE)
+## Function to plot
+
+pmap(
+	sample.combinations,
+	function(sample_1, sample_2) {
+		p <- ggplot(log2.TMM, aes_string(x=sample_1, y=sample_2)) +
+			geom_point(size=0.5, color="#34698c") +
+			geom_abline(slope=1, intercept=0, lty=2) +
+			theme_bw() +
+			labs(
+				x=paste0("log2(", sample_1, "+1)"),
+				y=paste0("log2(", sample_2, "+1)")
+			)
+
+		ggsave(
+			file.path("TSR_correlation", paste0(sample_1, "_vs_", sample_2, ".tiff")),
+			plot=p, device="tiff", width=5, height=5
+		) 
+	}
 )
-
-## Calculate jaccard index per decile.
-
-find.jaccard <- function(x, y) {
-	x <- makeGRangesFromDataFrame(x, keep.extra.columns=TRUE)
-	y <- makeGRangesFromDataFrame(y, keep.extra.columns=TRUE)
-
-	intersects <- GenomicRanges::intersect(x, y, ignore.strand = FALSE)
-	intersection <- intersects %>% width %>% sum
-
-	union <- GenomicRanges::union(x, y, ignore.strand = FALSE) %>%
-		width %>% sum
-
-	ans <- DataFrame(
-		intersection,
-		union,
-		jaccard = intersection/union,
-		n_intersections = length(intersects)
-	)
-
-	return(ans)
-}
